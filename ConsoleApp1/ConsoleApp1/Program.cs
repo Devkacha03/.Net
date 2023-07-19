f7cdb49076abc367fd36ab6df68a42a305f905a9
dev kacha <devkacha90@gmail.com>
2023-07-19 22:18:34 +0530

    program created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int num1;
+            int num2;
+            int num3;
+            int sum;
+            Console.WriteLine("Enter 3[integer] number ");
+
+            Console.Write("Enter first number :");
+            num1=int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number :");
+            num2 =int.Parse(Console.ReadLine());
+
+            Console.Write("Enter third number :");
+            num3 =int.Parse(Console.ReadLine());
+
+            sum=num1 + num2 + num3;
+
+            Console.WriteLine("sum is"+sum);
+            Console.ReadLine();
+            
+
+            string Company_Name, Company_address, Company_PhoneNo, Company_FaxNo, Company_WebSite, Manager_FirstName, Manager_LastName, Manager_PhoneNo, Manager_age;
+            //  byte mage;
+
+            string[] var = new string[] { "Company_Name", "Company_address", "Company_PhoneNo", "Company_FaxNo", "Company_WebSite", "Manager_FirstName", "Manager_LastName", "Manager_PhoneNo", "Manager_age" };
+
+            for (int i = 0; i < var.Length; i++)
+            {
+                Console.Write("Enter " + var[i] + " --> ");
+                var[i] = Console.ReadLine();
+            }
+            Console.WriteLine("------------------------------------------------------");
+            for (int i = 0; i < var.Length; i++)
+            {
+                Console.WriteLine(var[i]);
+                Console.WriteLine("------------------------------------------------------");
+
+            }
+
+
+            Console.ReadLine();
+            
+            //A company has name, address, phone number, fax number, web site and manager.
+            //The manager has first name, last name, age and a phone number.
+            //Write a program that reads the information about a company and its manager and prints them on the console.
+
+            String comname,comadd,comwebname,manfname,manlname;
+            long comphone,manphone;
+            int comfaxnum,manage;
+            
+            Console.WriteLine("Enter Company details");
+            Console.Write("Enter Company Name-->");
+            comname = Console.ReadLine();
+
+            Console.Write("Enter Company Address -->");
+            comadd = Console.ReadLine();
+
+            Console.Write("Enter Company Phone Number-->");
+            comphone = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter Company fax Number-->");
+            comfaxnum = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter Company Website Name -->");
+            comwebname = Console.ReadLine();
+
+
+            Console.WriteLine("Enter Manager Details");
+
+            Console.Write("Enter Manager FirstName-->");
+            manfname = Console.ReadLine();
+
+            Console.Write("Enter Manager LastName-->");
+            manlname = Console.ReadLine();
+
+            Console.Write("Enter Manager Age-->");
+            manage = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter Manager Phone Number-->");
+            manphone = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nCompany Information:");
+            Console.WriteLine("Name: " + comname);
+            Console.WriteLine("Address: " + comadd);
+            Console.WriteLine("Phone number: " + comphone);
+            Console.WriteLine("Fax number: " + comfaxnum);
+            Console.WriteLine("Website: " + comwebname);
+
+            Console.WriteLine("\nManager Information:");
+            Console.WriteLine("First name: " + manfname);
+            Console.WriteLine("Last name: " + manlname);
+            Console.WriteLine("Age: " + manage);
+            Console.WriteLine("Phone number: " + manphone);
+
+
+            Console.ReadLine();
+            */
+
+        }
+    }
+}
